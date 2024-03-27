e50dd69b4d698d1fe6779661145f92cefb6d1e17
unknown <marcofacundolucas@gmail.com>
2024-03-27 11:45:13 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import commonjs from 'rollup-plugin-commonjs'
+
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     commonjs()
+//   ],
+//   base:"https://facundomarcobacigalupo.github.io/Blog",
+//   build: {
+//     commonjsOptions: {
+//       include: [/@workspace\/ckeditor5-custom-build/, /node_modules/],
+//       exclude: ['ckeditor5-custom-build']
+//     }
+//   },
+//   optimizeDeps: {
+//     include: ['@workspace/ckeditor5-custom-build','ckeditor5-custom-build']
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from 'rollup-plugin-commonjs'
-
 
 export default defineConfig({
   plugins: [
-    react(),
-    commonjs()
+    react()
   ],
-  build: {
-    commonjsOptions: {
-      include: [/@workspace\/ckeditor5-custom-build/, /node_modules/],
-      exclude: ['ckeditor5-custom-build']
-    }
-  },
+  base: "https://facundomarcobacigalupo.github.io/Blog",
   optimizeDeps: {
     include: ['@workspace/ckeditor5-custom-build','ckeditor5-custom-build']
   }
